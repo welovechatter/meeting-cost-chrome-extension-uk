113f0d77ff090a0e8a229a3c0e389a527840e31d
Paul <paul@meetric.app>
2021-11-29 15:31:28 +0100

    experiemented with content scrips - stuck

--- content.js
+++ content.js
@@ -1,6 +1,46 @@
-var meetingGuest = document.getElementsByClassName('JAPzS');
-console.log(meetingGuest);
-for (var i = 0, l = meetingGuest.length; i < l; i++) {
-    console.log(meetingGuest[i]);
-    meetingGuest[i].innerHTML = "A LOT";
-}
+window.addEventListener('load', function() {
+    
+    
+    function findNode() {
+        var testContentOne = document.getElementsByClassName('CwaK9');
+
+        if(testContentOne != undefined) {
+            var testContentTwo = document.getElementsByClassName('RKLVef'); 
+            for (var i = 0, l = testContentTwo.length; i < l; i++) {
+                console.log(testContentTwo[i]);
+                testContentTwo[i].innerHTML = "TEST";
+            }
+        } else {
+            setTimeout(findNode(),1000)
+        }
+    }
+
+    findNode();
+
+}, false);
+
+
+// https://www.smashingmagazine.com/2019/04/mutationobserver-api-guide/
+
+
+// window.addEventListener('load', function() {
+
+// const observer = new MutationObserver(() => {
+//     sayHello();
+//   });
+
+// function sayHello() {
+//     if(document.getElementsByClassName('RveJvd.snByac')) {
+//         var testContent = document.getElementsByClassName('EfQccc')
+//         testContent[0].insertAdjacentHTML('beforeend',"HELLO");
+//         console.log("hello");
+//     }
+// }
+
+// const config = {
+//     childList: true,
+//     subtree: true,
+//   };
+//   observer.observe(document.body, config);
+  
+// }, false);
